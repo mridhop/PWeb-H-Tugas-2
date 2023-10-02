131c06359ec0d5b6ceb2a09261a3a635b7297fa6
Ridho Pratama <dhoable@gmail.com>
2023-10-02 21:07:50 +0700

    finish calculator-2

--- calculator-2/calculator.js
+++ calculator-2/calculator.js
@@ -1,11 +1,27 @@
-// calculator app
+let numField = document.getElementById("numfield");
+let resultText = document.getElementById("result");
 
-// get html elements
-let numsOrOps = document.querySelectorAll(".num");
-let equals = document.getElementById("calculate");
+// tulis angka/operator ke text field
+function addNum(value) {
+    if (numField.value === "0" && value !== ".") {
+        numField.value = value;
+    } else {
+        numField.value += value;
+    }
+}
 
+// hitung
+function calculateResult() {
+    try {
+        let result = eval(numField.value);
+        resultText.textContent = result
+    } catch (error) {
+        resultText.textContent = "Error";
+    }
+}
 
-// write numbers and operators to text field
-function addNum() {
-    
+// hapus field
+function clearField() {
+    numField.value = "";
+    resultText.textContent = "";
 }
